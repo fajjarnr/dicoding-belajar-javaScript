da61774268f8b60f31dacb53ee88af70bdc303ee
jay <jay@jay.localdomain>
2021-04-13 15:16:26 +0700

    first commit

---
+++ 1.JavascriptFundamentals/variableAndTipeData.js
@@ -0,0 +1,30 @@
+/**
+ * TODO:
+ * Buatlah variabel firstName, lastName, age, isMarried dengan ketentuan:
+ *  - firstName: bertipe data string, dengan nilai nama depan Anda.
+ *  - lastName: bertipe data string, dengan nilai nama belakang Anda.
+ *  - age: bertipe data number, dengan nilai umur Anda.
+ *  - isMarried: bertipe data boolean, dengan bebas Anda tentukan.
+ */
+
+// TODO
+const firstName = "Fajar";
+const lastName = "Nur Rohman";
+const age = 22;
+const isMarried = false;
+
+console.log(firstName);
+console.log(lastName);
+console.log(age);
+console.log(isMarried);
+
+/**
+ * Hiraukan kode di bawah ini
+ */
+
+module.exports = {
+  firstName,
+  lastName,
+  age,
+  isMarried,
+};
